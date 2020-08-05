aa379d9144b3b18412d7c2f03080194dfd0d704c
Tosin <mac@Tosin-MacBook-Air.local>
2020-08-05 13:15:30 -0400

    Handle Change and handle Submit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import React, { Component } from "react";
-import uuid from "uuid";
-import "bootstrap/dist/css/bootstrap.min.css";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 // showing vs-code github setup
 class App extends Component {
   state = {
-    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    items: [],
     id: uuid(),
-    item: "",
-    editItem: false
+    item: '',
+    editItem: false,
   };
-  handleChange = e => {
-    console.log("handle change");
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
   };
-  handleSubmit = e => {
-    console.log("handle Submit");
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+
+    const updateditems = [...this.state.items, newItem];
+
+    this.setState(
+      {
+        items: updateditems,
+        id: uuid(),
+        item: '',
+        editItem: false,
+      },
+      () => console.log(this.state.items)
+    );
   };
   clearList = () => {
-    console.log("clear list ");
+    console.log('clear list ');
   };
-  handleDelete = id => {
+  handleDelete = (id) => {
     console.log(`handle edit ${id}`);
   };
-  handleEdit = id => {
+  handleEdit = (id) => {
     console.log(`edit edit ${id}`);
   };
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-capitalize text-center">todo input</h3>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-5'>
+            <h3 className='text-capitalize text-center'>todo input</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
